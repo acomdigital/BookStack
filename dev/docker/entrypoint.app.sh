930fe171b308b5f0b38e592cf85cbe43dcf4af0d
denys.abramov <denys.abramov@marakas.digital>
2024-05-28 11:41:11 +0300

    update app entrypoint

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -11,5 +11,8 @@ else
     wait-for-it db:3306 -t 45
     php artisan migrate --database=mysql
     chown -R www-data:www-data storage
+    chown -R www-data:www-data public/uploads
+    chmod -R 775 storage
+    chmod -R 775 public/uploads
     exec apache2-foreground
 fi
